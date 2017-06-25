479fa90d8fcfbd419d410eec1cd3d6d3e74e3848
Flavio de Sousa <fl@viodesousa.com>
2017-06-25 20:58:29 +0200

    Now accepting "source files"...
    ...that should be completely empty.
    Otherwise an syntax error is triggered.

--- src/helloworlder.c
+++ src/helloworlder.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 
 int main(int argc, char **argv)
 {
+	switch (argc)
+	{
+		case 1: break;  // No source file required.
+		case 2:
+			{
+				FILE *fp = fopen(argv[1], "r");
+				if (!fp) {
+					// Non-existent files are valid source files. Beat that!
+					if (errno == ENOENT) break;
+					perror(argv[1]);
+					exit(2);
+				}
+				char buf;
+				int rc = fread(&buf, 1, 1, fp);
+
+				if (rc) {
+					// No statements, commands, comments supported
+					fprintf(stderr, "Syntax error: Unexpected statement\n");
+					exit(1);
+				}
+				fclose(fp);
+				break;
+			}
+	}
 	puts("Hello World!");
 	return 0;
 }
